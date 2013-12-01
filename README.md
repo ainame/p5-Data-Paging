a8081d726e3e42f9247a192a898c69368603a323
ainame <s.namai.2012@gmail.com>
2013-12-01 18:19:25 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Paging - It's new $module
+
+# SYNOPSIS
+
+    use Paging;
+
+# DESCRIPTION
+
+Paging is ...
+
+# LICENSE
+
+Copyright (C) ainame.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+ainame <s.namai.2012@gmail.com>
---
+++ lib/Paging.pm
@@ -0,0 +1,38 @@
+package Paging;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Paging - It's new $module
+
+=head1 SYNOPSIS
+
+    use Paging;
+
+=head1 DESCRIPTION
+
+Paging is ...
+
+=head1 LICENSE
+
+Copyright (C) ainame.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ainame E<lt>s.namai.2012@gmail.comE<gt>
+
+=cut
