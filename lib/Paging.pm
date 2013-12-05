381e844a969c4e6b3db98886932eb5ab9a69bff0
satoshi.namai <satoshi.namai@mixi.co.jp>
2013-12-05 18:55:00 +0900

    update pod

--- lib/Paging.pm
+++ lib/Paging.pm
@@ -42,15 +42,49 @@ __END__
 
 =head1 NAME
 
-Paging - It's new $module
+Paging - pagination helper for view
 
 =head1 SYNOPSIS
 
-    use Paging;
+use Paging;
+
+my $paging = Paging->create({
+    entries      => $entries,
+    total_count  => 100,
+    per_page     => 30,
+    current_page => 1,
+});
+
+$paging->has_next;    #=> TRUE
+$paging->has_prev;    #=> FALSE
+$paging->prev_page;   #=> 0
+$paging->next_page;   #=> 2
+$paging->begin_count; #=> 30
+$paging->end_count;   #=> 30
+...
+
+# If you use simple template engine like HTML::Template,
+# you should use Paging with renderer.
+my $paging = Paging->create({
+    entries      => $entries,
+    total_count  => 100,
+    per_page     => 30,
+    current_page => 1,
+}, '-NeighborLink');  # NeighborLink is the bundled renderer. You can load renderer like Plack::Middleware.
+
+$paging->render #=> output HASHREF value
 
 =head1 DESCRIPTION
 
-Paging is ...
+`Paging` is the helper library for implementation of paging.
+Especialy, `Paging` class is the factory class of Paging::Collection.
+
+Paging::Collection is the accessor of many pagination parameters like `Data::Page`.
+`Data::Page` make us implement paging ui, but that ui only simple paging.
+
+`Paging` always has next or prev page number. This feature difference from Data::Page' one.
+And, if you create a Paging::Collection's instance with `window` parameter,
+you can use this like `Data::Page::Navigation`.
 
 =head1 LICENSE
 
@@ -61,6 +95,6 @@ it under the same terms as Perl itself.
 
 =head1 AUTHOR
 
-ainame E<lt>s.namai.09@gmail.comE<gt>
+ainame E<lt>s.namai.2012@gmail.comE<gt>
 
 =cut
